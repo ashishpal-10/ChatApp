78c72b763ff14723a1a01ee93937fdc2b4de637d
Ashish Pal <90123404+ashishpal-10@users.noreply.github.com>
2026-09-13 10:39:09 +0530

    Fix baseURL formatting in axios instance

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatapp-backend-5qti.onrender.com
-/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chatapp-backend-5qti.onrender.com/api" : "/api",
   withCredentials: true,
 });
